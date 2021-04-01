ee15451acb1f0d18af131b727de154db780fac01
william-kl <hc740@scarletmail.rutgers.edu>
2021-04-01 17:48:42 -0400

    first components

---
+++ LogisticsApp/src/app/addserviceprovider/addserviceprovider.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormArray, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-addserviceprovider',
+  templateUrl: './addserviceprovider.component.html',
+  styleUrls: ['./addserviceprovider.component.css']
+})
+export class AddserviceproviderComponent implements OnInit {
+  // public SPForm:any;
+  // serviceprovider:any;
+  // errorMsg:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+               
+  }
+
+            
+           
+          
+            
+          
+}
--- LogisticsApp/src/app/app-routing.module.ts
+++ LogisticsApp/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddserviceproviderComponent } from './addserviceprovider/addserviceprovider.component';
+import { EditserviceproviderComponent } from './editserviceprovider/editserviceprovider.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LoginfailComponent } from './loginfail/loginfail.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { ServiceproviderdetailComponent } from './serviceproviderdetail/serviceproviderdetail.component';
+import { ServiceproviderlistComponent } from './serviceproviderlist/serviceproviderlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'serviceproviderlist', component: ServiceproviderlistComponent},
+  { path: 'serviceproviderdetail', component: ServiceproviderdetailComponent},
+  { path: 'editserviceprovider', component: EditserviceproviderComponent},
+  { path: 'addserviceprovider', component: AddserviceproviderComponent},
+  { path: 'forgotpassword', component: ForgotpasswordComponent},
+  { path: 'loginfail', component: LoginfailComponent },
+  { path: 'resetpassword', component: ResetpasswordComponent},
+  { path: "**", component: PagenotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  HomeComponent,
+  LoginComponent,
+  PagenotfoundComponent
+]
