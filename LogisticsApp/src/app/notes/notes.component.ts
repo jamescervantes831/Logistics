fb9d11b0f33eff8406110d6428dd28c30a2587cc
James Cervantes <jccervantes831@gmail.com>
2021-04-07 07:22:53 -0400

    Notes Component

--- LogisticsApp/src/app/notes/notes.component.ts
+++ LogisticsApp/src/app/notes/notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NotesService } from '../services/notes.service';
+import { SpService } from '../services/sp.service';
 
 @Component({
   selector: 'app-notes',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
-  constructor() { }
+  @Input() providerid: number;
+  public notes = []
+  constructor(private spService: SpService,
+    private notesService: NotesService) { }
 
   ngOnInit(): void {
+    let dataRows;
+    this.notesService.getNoteByProviderID(this.providerid).subscribe(
+      data => {
+        console.log('NOTES DATA: ' +JSON.stringify(data['data']['rows']))
+        dataRows = data['data']['rows']
+      },
+      error => console.log(error),
+      () => {
+        this.notes = dataRows
+        console.log("GET COMPLETE HERE ARE YOUR NOTES "+JSON.stringify(this.notes))
+      }
+    )
+    console.log("NOTES AFTER GET REQUEST" +this.notes)
   }
 
 }
